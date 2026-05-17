3d6983e6f6b7fffa13b2161c9ad6fdb81e50c4b3
Leo Accelerator <m15282536946@163.com>
2026-05-17 11:30:25 +0000

    Add missing Build import

--- app/src/main/java/com/leo/accelerator/LeoVpnService.kt
+++ app/src/main/java/com/leo/accelerator/LeoVpnService.kt
@@ -5,6 +5,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Intent
+import android.os.Build
 import android.os.Handler
 import android.os.Looper
 import android.net.VpnService
